6ca581ec4c16df70d74598724c6e65739134ba95
Ken Vilar <kenvilar@gmail.com>
2020-01-23 21:10:11 +0800

    Testing the cart

---
+++ tests/Feature/UpdateCartTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Product;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UpdateCartTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_can_update_cart_content()
+    {
+        $product = factory(Product::class)->create();
+
+        $response = $this->put('/cart/' . $product->id);
+
+        $response->assertRedirect('/cart');
+        $response->assertSessionHas('cart');
+
+        $cart = session('car');
+
+        $this->get('/cart')
+            ->assertSee($product->name)
+            ->assertSee($product->getPrice())
+            ->assertSee($cart->totalPrice());
+    }
+}

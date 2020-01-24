166d9455019771f3c9a3b0082dfdc5e7679c1935
Ken Vilar <kenvilar@gmail.com>
2020-01-24 21:44:14 +0800

    Add products()

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     protected $guarded = [];
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class)->withPivot('price');
+    }
 }

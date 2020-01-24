5f70a9f3648c1284c504153d6d29a4056c3cda4b
Ken Vilar <kenvilar@gmail.com>
2020-01-24 10:25:32 +0800

    PurchaseTest

---
+++ tests/Feature/PurchaseTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Cart;
+use App\Models\Product;
+use Faker\Provider\Payment;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PurchaseTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @test
+     */
+    public function it_can_purchase_products()
+    {
+        $product = factory(Product::class)->create([
+            'price' => 1000,
+        ]);
+        $cart = new Cart();
+        $cart->add($product, $product->id);
+
+        $payment = new FakePayment();
+
+        $this->post('/orders', [
+            'stripeEmail' => 'test@example.com',
+            'stripeToken' => $payment->getTestToken(),
+        ]);
+
+        $this->assertEquals('10.00', $payment->totalCharged());
+    }
+}

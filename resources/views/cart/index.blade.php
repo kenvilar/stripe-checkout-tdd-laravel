09ce5bc8b1143ff0012e52eecd79a0c038ccde21
Ken Vilar <kenvilar@gmail.com>
2020-01-24 09:31:15 +0800

    Modify cart index

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -1,10 +1,11 @@
+<h1>Cart Items</h1>
 @foreach($cart->items as $item)
     <div class="row">
         <div class="col-md-4">
-            <h4>{{$item->product->name}}</h4>
+            <h4>{{$item->name}}</h4>
         </div>
         <div class="col-md-4">
-            <h6>${{$item->product->getPrice()}}</h6>
+            <h6>${{$item->getPrice()}}</h6>
         </div>
     </div>
 @endforeach

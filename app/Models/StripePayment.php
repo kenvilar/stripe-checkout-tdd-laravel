c3cdb388993b75723a45c60ee45b35e9c9240652
Ken Vilar <kenvilar@gmail.com>
2020-01-28 14:08:00 +0800

    Create charge

--- app/Models/StripePayment.php
+++ app/Models/StripePayment.php
@@ -2,7 +2,21 @@
 
 namespace App\Models;
 
-class StripePayment
+use App\Contracts\PaymentContract;
+use Stripe\Charge;
+
+class StripePayment implements PaymentContract
 {
-    //
+    public function charge($total, $token)
+    {
+        // `source` is obtained with Stripe.js; see https://stripe.com/docs/payments/accept-a-payment-charges#web-create-token
+        Charge::create([
+            'amount' => $total,
+            'currency' => 'usd',
+            'source' => $token,
+            'description' => 'My First Test Charge (created for API docs)', //optional
+        ], [
+            'api_key' => config('services.stripe.secret'),
+        ]);
+    }
 }

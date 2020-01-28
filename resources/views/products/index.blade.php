9f00d5ee597bbc9de68b09211fe53cd71d9f0d32
Ken Vilar <kenvilar@gmail.com>
2020-01-29 00:57:01 +0800

    Product list page

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,7 +1,38 @@
-<h1>Products</h1>
-@foreach($products as $product)
-    <div>
-        <h4>{{ $product->name }}</h4>
-        <p>{{ $product->getPrice() }}</p>
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            @foreach($products as $product)
+                <div class="col-md-3">
+                    <div class="img-thumbnail">
+                        <img src="http://via.placeholder.com/400x400" alt="image" class="embed-responsive">
+                        <div class="figure-caption">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <h4>{{ $product->name }}</h4>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <p>${{ $product->getPrice() }}</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <form action="/cart/{{$product->id}}" method="post">
+                                        {{csrf_field()}}
+                                        {{method_field('PUT')}}
+                                        <button class="btn btn-success">
+                                            Add to cart
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
-@endforeach
+@endsection

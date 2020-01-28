6b540af51bba1b68eb4f74ad12399de1db6d4493
Ken Vilar <kenvilar@gmail.com>
2020-01-28 14:49:12 +0800

    Asserts the number of elements of an array

--- tests/Unit/StripePaymentTest.php
+++ tests/Unit/StripePaymentTest.php
@@ -44,7 +44,7 @@ class StripePaymentTest extends TestCase
 
         $payment->charge(1000, $token->id);
 
-        $this->assertEquals(1, $this->newCharges());
+        $this->assertCount(1, $this->newCharges());
         $this->assertEquals(1000, $this->lastCharge()->amount);
     }
 
